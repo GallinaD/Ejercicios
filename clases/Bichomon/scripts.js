6d2f7288f6e9891c81c087188f59b597ba9047f3
GallinaD <je.arias99@gmail.com>
2024-01-24 13:54:47 +0100

    muchas cosas

---
+++ clases/Bichomon/scripts.js
@@ -0,0 +1,7 @@
+console.log(document.URL);
+
+document.querySelector('#gen-1').innerHTML = "Generasión 1 Pokimon";
+
+document.querySelector('.infocard-list', '.infocard-list-pkmn-lg').style.background = "pink";
+
+document.querySelectorAll('img')[0].setAttribute('src', 'https://media.giphy.com/media/2v170e71aanfi/giphy.gif');
