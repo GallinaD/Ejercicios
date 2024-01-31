d757f3ed534777d3eddf418cbe8e4cffd08a3c12
GallinaD <je.arias99@gmail.com>
2024-01-31 13:52:22 +0100

    hexagono del demonio

---
+++ clases/enero/31-01-ejer_rombo/script.js
@@ -0,0 +1,17 @@
+let lienzo = "";
+for (let i = 2; i <= 5; i++) {
+    lienzo += "\n";
+
+    for (let j = 0; j < i; j++) {
+        lienzo += "+";
+    }
+
+} alert(lienzo);
+
+const hexagono = [
+    [4, 10, '+', ' '],
+    [2, 8, '+', ' '],
+    [1, 3, '+', ' '],
+    [3, 1, '+', ' ']
+  ];
+
