6c9ca4255a77968723c036c75effbc5c47c876bb
GallinaD <je.arias99@gmail.com>
2024-01-31 22:20:05 +0100

    sin mucho que comentar

--- clases/enero/31-01-ejer_rombo/script.js
+++ clases/enero/31-01-ejer_rombo/script.js
@@ -1,12 +1,52 @@
-let lienzo = "";
-for (let i = 2; i <= 5; i++) {
-    lienzo += "\n";
+// 4 columnas; 10 filas
 
-    for (let j = 0; j < i; j++) {
-        lienzo += "+";
+// ---++++---     3 espacios 4 simbolos
+// --++++++--     2 espacios 6 simbolos
+// -++++++++-     1 espacio  8 simbolos
+// ++++++++++     10 simbolos
+
+let arreglo = "";
+
+for (let i = 0; i < array.length; i++) {
+    
+    
+}
+
+
+
+
+
+
+
+
+/*
+function hexagono() {
+  var dibujo = "";
+  for (var i = 2; i <= 5; i++) {
+    for (var s = 5 - 1; s >= i; s--) {
+      dibujo += (" ");
     }
+    for (var j = 1; j <= i; j++) {
+      dibujo += ("+")
+    }
+    dibujo += "\n";
+  }
+  if (i == 5 + 1) {
+    for (var i = 1; i < 5 - 1; i++) {
+      for (var s = 1; s <= i; s++) {
+        dibujo += (" ");
+      }
+      for (j = i; j <= 5 - 1; j++) {
+        dibujo += ("+");
+      }
+      dibujo += "\n";
+    }
+  }
+  alert(dibujo);
+}
+hexagono();
 
-} alert(lienzo);
+
 
 const hexagono = [
     [4, 10, '+', ' '],
@@ -14,4 +54,5 @@ const hexagono = [
     [1, 3, '+', ' '],
     [3, 1, '+', ' ']
   ];
+*/
 
