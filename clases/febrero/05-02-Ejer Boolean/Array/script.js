bec8983e3ffe0e7d518c20596af215a9a5b7756d
GallinaD <je.arias99@gmail.com>
2024-02-05 13:43:06 +0100

    cosas

---
+++ clases/febrero/05-02-Ejer Boolean/Array/script.js
@@ -0,0 +1,8 @@
+let arreglo1 = [
+    [1,"hola", 3]
+    ["adios", 3, 2]
+    [4, "panita", 5]
+];
+
+
+
---
+++ clases/febrero/05-02-Ejer Boolean/Boolean/script.js
@@ -0,0 +1,12 @@
+var a = [1,2,-1,4,6,6];
+var b = [1,2,3,4,6,6];
+var condicion1 = a[0]>a[2]&&a[1]<a[3]&&a[4]==a[5];
+var condicion2 = b[0]>b[2]&&b[1]<b[3]&&b[4]==b[5];
+var result1 = new Boolean(condicion1);
+
+if(result1.valueOf){
+  console.log("Esta ordenado");
+}
+
+//console.log(new Boolean(condicion1));
+//console.log(new Boolean(condicion2));
