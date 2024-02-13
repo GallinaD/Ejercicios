1e3f2e843979e3cf3b027deca7f267feba2d906d
GallinaD <je.arias99@gmail.com>
2024-02-13 13:54:26 +0100

    perros encerrados

--- clases/febrero/12-02-Ejer-promesas-Perros/Perro-API/script.js
+++ clases/febrero/12-02-Ejer-promesas-Perros/Perro-API/script.js
@@ -1,18 +1,48 @@
-function buscar(){
-    
+function buscar() {
+
     fetch('https://dog.ceo/api/breeds/image/random')
-    .then(res=>res.json())
-    .then(json=>{
-        console.log(json)
+        .then(res => res.json())
+        .then(json => {
+            console.log(json)
 
-        let dogUrl = json.message;        
+            let dogUrl = json.message;
 
-        let dogImg = document.createElement("img");
+            let dogImg = document.createElement("img");
 
-        dogImg.setAttribute("src", dogUrl);
-        dogImg.setAttribute("width", "200px");
-        dogImg.setAttribute("height", "200px");
-        document.body.appendChild(dogImg);
+            dogImg.setAttribute("src", dogUrl);
+            dogImg.setAttribute("width", "150px");
+            dogImg.setAttribute("height", "150px");
+            document.body.appendChild(dogImg);
 
-    });
+        });
 }
+
+function tabla() {
+
+    let tabla = document.createElement("table");
+    document.body.appendChild(tabla);
+    tabla.setAttribute("border", "solid black 5px")
+
+    for (let i = 0; i < 4; i++) {
+
+        let trow = document.createElement("tr");
+        tabla.appendChild(trow);
+
+        for (let j = 0; j < 5; j++) {
+
+            let tdata = document.createElement("td");
+            trow.appendChild(tdata);
+            tdata.setAttribute("width", "150px");
+            tdata.setAttribute("height", "150px");
+
+        }
+    }
+
+    let boton = document.createElement("button");
+    document.body.appendChild(boton);
+    boton.setAttribute("onclick", "buscar()")
+    boton.innerHTML = "Let de dogs out";
+}
+
+
+tabla()
