1173deb2497514d3a5e3849de399e12e82945236
GallinaD <je.arias99@gmail.com>
2024-02-14 13:52:42 +0100

    examen

--- clases/Ejer_Examen_2/script.js
+++ clases/Ejer_Examen_2/script.js
@@ -1,59 +1,153 @@
 // Crear el formulario al cargar la página
-document.addEventListener("DOMContentLoaded", () => {   
-    var formulario = document.createElement("form");
 
-    formulario.setAttribute('style', "width:300px;margin: 15% auto");
+document.addEventListener("DOMContentLoaded", () => {
+  var formulario = document.createElement("form");
 
-    makeForm("Nombre", formulario);
-    makeForm("Edad", formulario);
-    makeForm("Asignatura", formulario);
+  formulario.setAttribute('style', "width:300px;margin: 15% auto");
 
-    var inputBoton = document.createElement("input");
-    inputBoton.innerHTML="Enviar"
-    inputBoton.setAttribute("id", "Aceptar");
-    inputBoton.setAttribute("type", "button");
-    inputBoton.setAttribute("value", "Aceptar");
-    formulario.appendChild(inputBoton);
+  makeForm("Nombre", formulario);
+  makeForm("Edad", formulario);
+  makeForm("Asignatura", formulario);
 
-    document.body.appendChild(formulario);
+  var inputBoton = document.createElement("input");
+  inputBoton.innerHTML = "Enviar"
+  inputBoton.setAttribute("id", "Aceptar");
+  inputBoton.setAttribute("type", "button");
+  inputBoton.setAttribute("value", "Aceptar");
+  formulario.appendChild(inputBoton);
 
+  document.body.appendChild(formulario);
+
+  // creamos los arrays que guarden la información de los alumnos
+  let arrayNom = [];
+  let arrayEdad = [];
+  let arrayAsig = [];
+
+
+  inputBoton.addEventListener("click", () => {
+    // Obtenemos el valor de los inputs por el id
+
+    let valorNom = document.getElementById("NombreInput").value;
+    let valorEdad = document.getElementById("EdadInput").value;
+    let valorAsig = document.getElementById("AsignaturaInput").value;
+
+    let getNombre = document.getElementById("NombreInput");
+    let getEdad = document.getElementById("EdadInput");
+    let getAsig = document.getElementById("AsignaturaInput");
+
+    // Tabla
+
+
+
+    //Comprobar validaciones y que sea distinto de 0
+
+    let okNombre = false;
+    let okEdad = false;    // Uso estas variables para que, al comprobar todos los datos sean true, lo guarde en un local storage
+    let okAsig = false;
+
+
+
+    if (valorNom != 0 && valNombre(valorNom)) {
+      getNombre.setAttribute("style", "border-color: green; width:100%;margin: 10px 0px;padding: 5px");
+      okNombre = true;
+    } else {
+      getNombre.setAttribute("style", "border-color: red; width:100%;margin: 10px 0px;padding: 5px");
+    }
+
+    if (valorEdad != 0 && valEdad(valorEdad)) {
+      getEdad.setAttribute("style", "border-color: green; width:100%;margin: 10px 0px;padding: 5px");
+      okEdad = true;
+    } else {
+      getEdad.setAttribute("style", "border-color: red; width:100%;margin: 10px 0px;padding: 5px");
+    }
+
+    if (valorAsig != 0 && valAsignatura(valorAsig)) {
+      getAsig.setAttribute("style", "border-color: green; width:100%;margin: 10px 0px;padding: 5px");
+      okAsig = true;
+    } else {
+      getAsig.setAttribute("style", "border-color: red; width:100%;margin: 10px 0px;padding: 5px");
+    }
+
+    if (okNombre && okEdad && okAsig) {
+      arrayNom.push(valorNom);
+      arrayEdad.push(valorEdad);
+      arrayAsig.push(valorAsig);
+
+      localStorage.setItem("Nombres", JSON.stringify(arrayNom));
+      localStorage.setItem("Edad", JSON.stringify(arrayEdad));
+      localStorage.setItem("Asignatura", JSON.stringify(arrayAsig));
+    }
+
+  })
 })
 
 // Función para crear los inputs del formulario
+
 function makeForm(nombre, formulario) {
 
-    let etiqueta = document.createElement("label");
-    let texto = document.createTextNode(nombre);
-    etiqueta.appendChild(texto);
+  let etiqueta = document.createElement("label");
+  let texto = document.createTextNode(nombre);
+  etiqueta.appendChild(texto);
 
-    var inputElemento = document.createElement("input");
-    inputElemento.setAttribute("type", "text");
-    inputElemento.setAttribute("id", nombre + "Input");
-    inputElemento.setAttribute("placeholder", nombre);
-    inputElemento.setAttribute("required", "true");
-    inputElemento.setAttribute("style", "width:100%;margin: 10px 0px;padding: 5px");
+  var inputElemento = document.createElement("input");
+  inputElemento.setAttribute("type", "text");
+  inputElemento.setAttribute("id", nombre + "Input");
+  inputElemento.setAttribute("placeholder", nombre);
+  inputElemento.setAttribute("required", "true");
+  inputElemento.setAttribute("style", "width:100%;margin: 10px 0px;padding: 5px");
 
-    formulario.appendChild(etiqueta);
+  formulario.appendChild(etiqueta);
 
-    formulario.appendChild(inputElemento);
-    var myBr = document.createElement("br");
-    formulario.appendChild(myBr);
+  formulario.appendChild(inputElemento);
+  var myBr = document.createElement("br");
+  formulario.appendChild(myBr);
 }
 
 // Validación del nombre (letras, mayúsculas, ñ/Ñ y espacios)
 
-
-
 function valNombre(nombre) {
-    for (let i = 0; i < nombre.length; i++) {
-      var char = nombre.charCodeAt(i);
-  
-      let con1 = char >= 65 && char <= 90;
-      let con2 = char >= 97 && char <= 122;
-      let con3 = char == 209 || char == 241 || char == 32;
-  
-      if (!(con1 || con2 || con3)) return false;
-    }
-  
-    return true;
+  for (let i = 0; i < nombre.length; i++) {
+    let char = nombre.charCodeAt(i);
+
+    let con1 = char >= 65 && char <= 90;
+    let con2 = char >= 97 && char <= 122;
+    let con3 = char == 209 || char == 241 || char == 32;
+
+    if (!(con1 || con2 || con3)) return false;
   }
+
+  return true;
+}
+
+// Validación edad (números enteros)
+
+function valEdad(edad) {
+  for (let i = 0; i < edad.length; i++) {
+    let char = edad.charCodeAt(i);
+
+    let con = char >= 48 && char <= 57;
+
+    if (!con) { return false }
+
+  }
+
+  return true;
+}
+
+// Validación asignatura (letras y números)
+
+function valAsignatura(asignatura) {
+  for (let i = 0; i < asignatura.length; i++) {
+    let char = asignatura.charCodeAt(i);
+
+    let con1 = char >= 48 && char <= 57;
+    let con2 = char >= 65 && char <= 90;
+    let con3 = char >= 97 && char <= 122;
+    let con4 = char == 209 || char == 241;
+
+    if (!(con1 || con2 || con3 || con4)) return false;
+  }
+
+  return true;
+}
+
